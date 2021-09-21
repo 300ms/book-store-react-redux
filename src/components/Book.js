9d727ac1b710512034699ffacc152fbc551f460b
300ms <ilhansnmz44@gmail.com>
2021-09-21 19:03:26 +0300

    Edit Bookform

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,22 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { removeBook } from '../actions';
+
+const handleRemoveBook = (book) => {
+  removeBook(book.id);
+};
 
 const Book = (props) => {
   const { book } = props;
+
+  // console.log(this.state);
   return (
     <tr>
       <td>{book.id}</td>
       <td>{book.category}</td>
       <td>{book.title}</td>
-      <td><button type="submit">Delete Book</button></td>
+      <td><button type="submit" onClick={handleRemoveBook}>Delete Book</button></td>
     </tr>
   );
 };
--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,46 +1,77 @@
 import React from 'react';
+import { addBook } from '../actions';
 
-const BooksForm = () => {
-  const categories = [
-    'Action',
-    'Biography',
-    'History',
-    'Horror',
-    'Kids',
-    'Learning',
-    'Sci-Fi',
-  ];
-  return (
-    <div className="booksForm">
-      <form className="book-form" action="#">
-        <h3>
-          Create new book
-        </h3>
-        <br />
-        <div className="form-fields">
-          <label htmlFor="title">
-            Book Title:
+const categories = [
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
 
-            <input type="text" id="title" name="title" />
-          </label>
+export default class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: null,
+      category: null,
+    };
 
-          <label htmlFor="category">
-            Category:
-            <select name="category" id="category">
-              {
-                    categories.map((cat, id) => (
-                      <option key={`opt_${id * 2}`} value={id}>{cat}</option>
-                    ))
-                }
-            </select>
-          </label>
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-          <button type="submit">Add Book</button>
-        </div>
-      </form>
-    </div>
+  handleChange(event) {
+    if (event.target.nodeName === 'INPUT') {
+      const newState = { ...this.state, title: event.target.value };
+      this.setState(newState);
+    } else if (event.target.nodeName === 'OPTION') {
+      const newState = { ...this.state, category: event.target.value };
+      this.setState(newState);
+    }
 
-  );
-};
+    console.log(this.state);
+  }
 
-export default BooksForm;
+  handleSubmit(event) {
+    event.preventDefault();
+    addBook(this.state);
+    this.setState({ id: null, title: null, category: null });
+  }
+
+  render() {
+    return (
+      <div className="booksForm">
+        <form className="book-form" action="#">
+          <h3>
+            Create new book
+          </h3>
+          <br />
+          <div className="form-fields">
+            <label htmlFor="title">
+              Book Title:
+
+              <input type="text" id="title" name="title" onChange={this.handleChange} />
+            </label>
+
+            <label htmlFor="category">
+              Category:
+              <select name="category" id="category" onChange={this.handleChange}>
+                {
+                      categories.map((cat, id) => (
+                        <option key={`opt_${id * 2}`} value={id}>{cat}</option>
+                      ))
+                  }
+              </select>
+            </label>
+
+            <button type="submit" onClick={this.handleSubmit}>Add Book</button>
+          </div>
+        </form>
+      </div>
+
+    );
+  }
+}
